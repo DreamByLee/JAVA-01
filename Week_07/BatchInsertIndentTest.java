d6ee57f1ff7b08e7606dcacc186b783dde69e9e2
DreamByLee <coolliyijie@qq.com>
2021-03-02 15:51:28 +0800

    第7周作业

---
+++ Week_07/BatchInsertIndentTest.java
@@ -0,0 +1,78 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+/**
+ * @author Leeyj
+ * @date 2021/3/2 11:28
+ */
+public class BatchInsertIndentTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private String url = "jdbc:mysql://localhost:3306/week06?rewriteBatchedStatements=true";
+    private String user ="root";
+    private String password ="123";
+
+    @Test
+    public void batchInsert() {
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+//        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection(url, user, password);
+            Date createTime = new Date(System.currentTimeMillis());
+            String sql = "INSERT INTO indnetinfo(customerId,indentPrice,payTime,payType) VALUES(?,?,?,0)";
+
+            ps = conn.prepareStatement(sql);
+
+            long start = System.currentTimeMillis();
+
+            for (int i = 0; i < 1000000; i++) {
+                ps.setInt(1,i);
+                ps.setDouble(2,2.50);
+                ps.setDate(3, createTime);
+                ps.addBatch();
+            }
+            ps.executeBatch();
+            long end = System.currentTimeMillis();
+            System.out.println("耗时：" + (end - start));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testJdbcTempLate() throws SQLException {
+        Connection conn = jdbcTemplate.getDataSource().getConnection();
+
+        System.out.println(conn);
+    }
+
+
+}
